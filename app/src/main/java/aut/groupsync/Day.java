b32134fa3b2db6603d328c0e5d60f7b2e38a8ac7
Will <gogobebe2@gmail.com>
2019-08-28 11:44:34 +1200

    Update Day.java

--- app/src/main/java/aut/groupsync/Day.java
+++ app/src/main/java/aut/groupsync/Day.java
@@ -1,4 +1,18 @@
 package aut.groupsync;
 
 public class Day {
+	private List<Appointment> appointments;
+	private Date date;
+
+	public Day(Date date) {
+		this.date = date;
+	}
+
+	public List<Appointment> getAppointments() {
+		return this.appointments;
+	}
+
+	public Date getDate() {
+		return this.date;
+	}
 }
