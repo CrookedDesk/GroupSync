0128bd6397c309b95ae0491376b842b00f79acd1
Will <gogobebe2@gmail.com>
2019-08-28 11:45:42 +1200

    Update PersonalAppointment.java

--- app/src/main/java/aut/groupsync/PersonalAppointment.java
+++ app/src/main/java/aut/groupsync/PersonalAppointment.java
@@ -1,4 +1,18 @@
 package aut.groupsync;
 
-public class PersonalAppointment {
+public class PersonalAppointment extends Appointment {
+	private boolean isPrivate;
+
+	public PersonalAppointment(Date date, Note note, boolean hasAlarm, String name, boolean reoccuring, boolean isPrivate) {
+		super(date, note, hasAlarm, name, reoccuring);
+		this.isPrivate = isPrivate;
+	}
+
+	public void setPrivacy(boolean isPrivate) {
+		this.isPrivate = isPrivate;
+	}
+
+	public boolean isPrivate() {
+		return this.isPrivate;
+	}
 }
