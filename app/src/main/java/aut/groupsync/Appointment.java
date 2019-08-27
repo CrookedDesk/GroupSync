abba25adba660c7b3eb55fbd72189aa8ba2e7be7
Will <gogobebe2@gmail.com>
2019-08-28 11:44:08 +1200

    Update Appointment.java

--- app/src/main/java/aut/groupsync/Appointment.java
+++ app/src/main/java/aut/groupsync/Appointment.java
@@ -1,4 +1,45 @@
 package aut.groupsync;
 
 public class Appointment {
+	private Note note;
+	private Alarm alarm;
+	private String name;
+	private Date date;
+	private boolean reoccuring;
+
+	public Appointment(Date date, Note note, boolean hasAlarm, String name, boolean reoccuring) {
+		this.date = date;
+		this.note = note;
+		this.alarm = new Alarm(hasAlarm);
+		this.name = name;
+		this.reoccuring = reoccuring;
+	}
+
+	public Note getNote() {
+		return this.note;
+	}
+
+	public void setNote(Note note) {
+		this.note = note;
+	}
+
+	public Alarm getAlarm() {
+		return this.alarm;
+	}
+
+	public String getAppointmentName() {
+		return this.name;
+	}
+
+	public void setAppointmentName(String name) {
+		this.name = name;
+	}
+
+	public void setReoccuring(boolean reoccuring) {
+		this.reoccuring = reoccuring;
+	}
+
+	public boolean getReoccuring() {
+		return this.reoccuring;
+	}
 }
