5d70f290dbeb01e8c39931eced5c630753d716ef
Will <gogobebe2@gmail.com>
2019-08-28 11:45:11 +1200

    Update Note.java

--- app/src/main/java/aut/groupsync/Note.java
+++ app/src/main/java/aut/groupsync/Note.java
@@ -1,4 +1,17 @@
 package aut.groupsync;
 
 public class Note {
+	private String text;
+
+	public Note(String text) {
+		this.text = text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return this.text;
+	}
 }
