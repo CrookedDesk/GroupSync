af48ea5806cff69fe5847d88c9832dc5d49a3b1d
Will <gogobebe2@gmail.com>
2019-08-28 11:43:11 +1200

    Update Account.java

--- app/src/main/java/aut/groupsync/Account.java
+++ app/src/main/java/aut/groupsync/Account.java
@@ -10,11 +10,32 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Account extends Fragment {
+    private String username;
+	private String email;
+	private Timetable timetable;
+	private List<Group> groups = new ArrayList<Group>();
 
-    @Nullable
+/*    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.account_details, container, false);
-    }
+    }*/
+    
+    public Account(String username, String email) {
+		this.username = username;
+		this.email = email;
+	}
+
+	public Timetable getTimetable() {
+		return this.timetable;
+	}
+
+	public void setTimetable(Timetable timetable) {
+		this.timetable = timetable;
+	}
+
+	public List<Group> getGroups() {
+		return this.groups;
+	}
 }
 
