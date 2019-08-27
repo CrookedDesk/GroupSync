7e102852c16e58653c2f84002828a9c45a07a31f
Will <gogobebe2@gmail.com>
2019-08-28 11:46:06 +1200

    Update Week.java

--- app/src/main/java/aut/groupsync/Week.java
+++ app/src/main/java/aut/groupsync/Week.java
@@ -1,4 +1,15 @@
 package aut.groupsync;
 
 public class Week {
+	private List<Day> days;
+
+	public Week(List<Day> days) {
+		this.days = days;
+	}
+
+	public String getDateText() {
+		String year = days.get(0).getYear();
+		SimpleDateFormat weekFormat = new SimpleDateFormat("MM-dd");
+		return year + ", " + weekFormat.format(days.get(0)) + " to " weekFormat.format(days.get(6));
+	}
 }
