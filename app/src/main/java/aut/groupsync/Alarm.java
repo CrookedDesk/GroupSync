2f269ce77669c157bfc7c7a89333bc1b4498ac1b
Will <gogobebe2@gmail.com>
2019-08-28 11:43:33 +1200

    Update Alarm.java

--- app/src/main/java/aut/groupsync/Alarm.java
+++ app/src/main/java/aut/groupsync/Alarm.java
@@ -1,4 +1,26 @@
 package aut.groupsync;
 
 public class Alarm {
+	private boolean enabled;
+	
+	public Alarm(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Alarm() {
+		// Enable alarm by default
+		this(true)
+	}
+
+	public void enable() {
+		this.enabled = true;
+	}
+
+	public void disable() {
+		this.enabled = false;
+	}
+
+	public boolean isEnabled() {
+		return this.enabled;
+	}
 }
