c3993c1e8fe097bc5b8dca0392fe70ab873f1c5b
Mandeep Kaur <m.kaur.nz@gmail.com>
2019-09-13 23:21:02 +1200

    get google sign in to work

--- app/src/main/java/aut/groupsync/Account.java
+++ app/src/main/java/aut/groupsync/Account.java
@@ -1,44 +1,153 @@
 package aut.groupsync;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Account extends Fragment {
     private String username;
-	private String email;
-	private Timetable timetable;
-	private List<Group> groups = new ArrayList<Group>();
+    private String email;
+    private Timetable timetable;
+    private List<Group> groups = new ArrayList<Group>();
+    private SignInButton googleSignInButton;
+    private GoogleSignInClient googleSignInClient;
+    private static final String TAG = "AndroidClarified";
+    private Button signOutButton;
+    private TextView profileName, profileEmail;
+    private ImageView profileImage;
 
-/*    @Nullable
+
+    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.account_details, container, false);
-    }*/
-    
+        View view = inflater.inflate(R.layout.account_details, container, false);
+
+        googleSignInButton = view.findViewById(R.id.sign_in_button);
+        profileName = view.findViewById(R.id.profile_text);
+        profileEmail = view.findViewById(R.id.profile_email);
+        profileImage = view.findViewById(R.id.profile_image);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("862018706594-m8rp1jao3qktbr940bp9c8bal13dt14g.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = googleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, 101);
+            }
+        });
+
+        signOutButton = view.findViewById(R.id.sign_out_button);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+          Sign-out is initiated by simply calling the googleSignInClient.signOut API. We add a
+          listener which will be invoked once the sign out is the successful
+           */
+                googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        render(null);
+                    }
+
+                });
+            }
+        });
+
+        GoogleSignInAccount alreadyloggedAccount = GoogleSignIn.getLastSignedInAccount(getActivity());
+
+        render(alreadyloggedAccount);
+
+        return view;
+    }
+
+    private void render(GoogleSignInAccount googleSignInAccount) {
+        boolean signedIn = googleSignInAccount != null;
+        this.username = googleSignInAccount != null ? googleSignInAccount.getDisplayName() : "";
+        this.email = googleSignInAccount != null ? googleSignInAccount.getEmail() : "";
+
+        googleSignInButton.setVisibility(signedIn ? View.INVISIBLE : View.VISIBLE);
+        signOutButton.setVisibility(signedIn ? View.VISIBLE : View.INVISIBLE);
+        profileName.setText(username);
+        profileEmail.setText(email);
+
+        if (googleSignInAccount != null) {
+
+            Picasso.get().load(googleSignInAccount.getPhotoUrl()).centerInside().fit().into(profileImage);
+        } else {
+            profileImage.setImageBitmap(null);
+        }
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode) {
+                case 101:
+                    try {
+                        // The Task returned from this call is always completed, no need to attach
+                        // a listener.
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        render(account);
+                    } catch (ApiException e) {
+                        // The ApiException status code indicates the detailed failure reason.
+                        Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+                    }
+                    break;
+            }
+    }
+
     public Account(String username, String email) {
-		this.username = username;
-		this.email = email;
-	}
+        this.username = username;
+        this.email = email;
+    }
 
-	public Timetable getTimetable() {
-		return this.timetable;
-	}
+    public Timetable getTimetable() {
+        return this.timetable;
+    }
 
-	public void setTimetable(Timetable timetable) {
-		this.timetable = timetable;
-	}
+    public void setTimetable(Timetable timetable) {
+        this.timetable = timetable;
+    }
 
-	public List<Group> getGroups() {
-		return this.groups;
-	}
+    public List<Group> getGroups() {
+        return this.groups;
+    }
 }
 
